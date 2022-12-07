63d302e8f33ce109874aee9294361c1687992995
Florian <sicilia.florian02@gmail.com>
2022-12-07 22:38:43 +0100

    chap 3

--- chap3_composant_props/src/App.jsx
+++ chap3_composant_props/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+export function ComponentProps(props) { // props OU destructuration { prop1, prop2 }
+    return (
+        <div>
+            <h1>je suis { props.firstname } et j'ai { props.age }ans</h1>
+            <button onClick={props.sayHello}>dit bonjour</button>
+        </div>
+    )
 }
 
-export default App
+export function ComponentChildrenAndProps({ name, children }) {
+    console.log(children); // Les éléments enfant qu'on passe au composant lors de l'appel de celui-ci (comme les slots)
+    console.log(name); // une props
+
+    return (
+        <button>
+            { children } - { name }
+        </button>
+    )
+}
+
+function App() {
+    const presentation = () => console.log("Salut");
+
+    return (
+        <>
+            <ComponentProps firstname="Florian" age={20} sayHello={ presentation } />
+
+            <ComponentChildrenAndProps name="Toto">
+                <span>Clique</span>
+                <span>&#9824;</span>
+            </ComponentChildrenAndProps>
+        </>
+    )
+}
+
+export default App;
